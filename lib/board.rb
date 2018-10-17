be75d773dc79e2c97d164cdeee5f37d7afc02785
Sean  Febo <seanfebo@techie.com>
2018-10-17 22:24:28 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,7 @@
 class Board 
-  attr_accessor :cells, :token
-  def initialize
+  attr_accessor :cells
+  
+  def initialize 
     reset!
   end
   
@@ -8,62 +9,52 @@ class Board
     @cells = Array.new(9, " ")
   end
   
-  def display
-  puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
-  puts "-----------"
-  puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
-  puts "-----------"
-  puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  def display 
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
   end
   
   def position(num)
-    number = num.to_i - 1
-    if number.between?(0,8)
-    cells[number]
+    number = num.to_i - 1 
+    if number.between?(0,8) 
+      @cells[number]
     end
   end
   
   def full?
-    @cells.all? do |space|
-      space != " "
-    end
+    @cells.all?{|s| s != " "}
   end
   
   def turn_count 
-    count = 0 
-    @cells.each do |space|
-      if space == "X" || space == "O"
-        count = count + 1
-      end
-    end
-    count 
+    @cells.count{|s| s != " "}
   end
   
   def taken?(num)
     number = num.to_i - 1
-    if number <= 8 && number >= 0
-    if cells[number] == "X" || cells[number] == "O"
-      return true 
-    else 
+    if @cells[number] != " "
+      true 
+    else
       false 
-      end
     end
   end
   
   def valid_move?(num)
-    number = num.to_i - 1
-    if @cells[number] == " " && number.between?(0,8)
-    return true
+    if num.to_i.between?(1, 9) && !taken?(num)
+      true
+    else
+      false 
     end
   end
   
-  def update(input, player)
-    number = input.to_i - 1 
+  def update(num, player)
+    if valid_move?(num)
+      number = num.to_i - 1 
       @cells[number] = player.token
+    end
   end
-    
-    
-  
-  
+      
   
 end
--- lib/players/human.rb
+++
@@ -1,9 +0,0 @@
-module Players 
-  class Human < Player 
-    
-    def move(board)
-      puts "Please enter 1-9"
-     input = gets.strip 
-    end
-  end
-end
